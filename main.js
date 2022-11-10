40ca01a6d6c3bad31272774e0d7308f4b99c067e
glauber castro miranda <glauber.happy@gmail.com>
2022-11-10 11:37:55 -0300

    "bkup"

--- main.js
+++ main.js
@@ -10,14 +10,22 @@ form.addEventListener('submit',function (e) {
 function add_linha () {
     const input_nome = document.getElementById('input-nome').value
     const input_nota = document.getElementById('input-nota').value
-    const status = input_nota >= 7 ? "aprovado" : "reprovado"
-    const msg_alt = input_nota >= 7 ? `o exercicio ${input_nome}, foi ${status}, parabaramens.` : `o exercicio ${input_nome}, foi ${status} por ${10-input_nota} pontos.`
+    let st = input_nota >= 7 ? "aprovado" : "reprovado"
+    let icon = ''
+    if (input_nota == 10) {
+        icon = 'estrela.webp'
+    } else if (icon >= 7) {
+        icon = 'aprovado.png'
+    } else {
+        icon = 'reprovado.png'
+    }
+    const msg_alt = input_nota >= 7 ? `o exercicio ${input_nome}, foi ${st}, parabaramens.` : `o exercicio ${input_nome}, foi ${status} por ${10-input_nota} pontos.`
     const nota_m = document.getElementById("nota-media")
     let linha = tabela.innerHTML
     linha += `<tr>
     <td>${input_nome}</td>
     <td>${input_nota}</td>
-    <td><img src="images/${status}.png" alt="${msg_alt}"></td>
+    <td><img src="images/${icon}" alt="${msg_alt}"></td>
 </tr><br>`
     notas.push(parseInt(input_nota))
     tabela.innerHTML = linha
@@ -25,12 +33,12 @@ function add_linha () {
 function mediar () {
     let sum = 0
     
-    for (let c = 0;c > notas.length; c++) {
-        sum = sum + notas[c]
+    for (let c = 0;c < notas.length; c++) {
+        console.log(`${sum} ${notas} ${notas[c]}`)
+        sum += notas[c]
+        
     } 
     let med = sum / notas.length
-    console.clear()
-    console.log(notas)
-    console.log(med)
-    console.log(sum)
+    const status = med >= 7
+    
 }
